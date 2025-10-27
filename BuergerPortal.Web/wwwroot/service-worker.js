2ac0d26885565f51fecfefc5160548e091cb7a22
Sponsel, Lukas (ZBFS) <lukas.Sponsel@zbfs.bayern.de>
2025-10-27 08:59:40 +0100

    ja

---
+++ BuergerPortal.Web/wwwroot/service-worker.js
@@ -0,0 +1,15 @@
+﻿/* Basic SW: Cache Shell + Fetch passthrough */
+const CACHE = "cp-cache-v1";
+const ASSETS = ["/", "/css/site.css"];
+
+self.addEventListener("install", (e) => {
+    e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
+});
+
+self.addEventListener("activate", (e) => { self.clients.claim(); });
+
+self.addEventListener("fetch", (e) => {
+    e.respondWith(
+        caches.match(e.request).then(r => r || fetch(e.request))
+    );
+});
