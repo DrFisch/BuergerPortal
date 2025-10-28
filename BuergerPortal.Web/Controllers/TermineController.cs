38d9c8478efc7c5e9f0471cef09f8a9283a3656f
Sponsel, Lukas (ZBFS) <lukas.Sponsel@zbfs.bayern.de>
2025-10-28 14:43:45 +0100

    arbeit an den views der web app

---
+++ BuergerPortal.Web/Controllers/TermineController.cs
@@ -0,0 +1,55 @@
+﻿using BuergerPortal.Web.Features.Termine;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BuergerPortal.Web.Controllers
+{
+    public class TermineController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            // TODO: Später aus Application-Layer laden (aktueller Benutzer)
+            var vm = new TermineIndexVm
+            {
+                Termine = new()
+                {
+                    new TerminListItemVm
+                    {
+                        Id = Guid.NewGuid(),
+                        Dienst = "Ausweis beantragen",
+                        Datum = DateTime.Today.AddDays(2),
+                        Uhrzeit = "09:30",
+                        Standort = "Bürgeramt Mitte",
+                        Storniert = false
+                    },
+                    new TerminListItemVm
+                    {
+                        Id = Guid.NewGuid(),
+                        Dienst = "Meldebescheinigung",
+                        Datum = DateTime.Today.AddDays(5),
+                        Uhrzeit = "11:00",
+                        Standort = "Bürgeramt Süd",
+                        Storniert = false
+                    }
+                }
+            };
+            return View(vm);
+        }
+
+        [HttpGet]
+        public IActionResult Buchen()
+        {
+            // Nächster Schritt: eigene Buchungsseite
+            return View(); // (erstmal leere View – wir bauen sie im nächsten Schritt)
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Stornieren(Guid id)
+        {
+            // TODO: Application-Layer Command aufrufen (CancelAppointmentCommand)
+            TempData["BookingSuccess"] = "Termin wurde storniert (Mock).";
+            return RedirectToAction(nameof(Index));
+        }
+    }
+}
---
+++ BuergerPortal.Web/Features/Termine/TermineIndexVm.cs
@@ -0,0 +1,17 @@
+﻿namespace BuergerPortal.Web.Features.Termine
+{
+    public sealed class TermineIndexVm
+    {
+        public List<TerminListItemVm> Termine { get; set; } = new();
+    }
+
+    public sealed class TerminListItemVm
+    {
+        public Guid Id { get; set; }
+        public string Dienst { get; set; } = "";
+        public DateTime Datum { get; set; }    // nur Datum nutzen wir in der View
+        public string Uhrzeit { get; set; } = ""; // "HH:mm"
+        public string Standort { get; set; } = "";
+        public bool Storniert { get; set; }
+    }
+}
