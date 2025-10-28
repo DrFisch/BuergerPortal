39c3c93d8780a82115842b6a8399acf75e732dac
Sponsel, Lukas (ZBFS) <lukas.Sponsel@zbfs.bayern.de>
2025-10-28 06:43:27 +0100

    authserver packages + OpenIdDict + DB

--- AuthenticationServer/Data/ApplicationDbContext.cs
+++ AuthenticationServer/Data/ApplicationDbContext.cs
@@ -1,13 +1,23 @@
-﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+
 
 namespace AuthenticationServer.Data
 {
-    public class ApplicationDbContext : IdentityDbContext
+    public class ApplicationUser : IdentityUser
+    {
+    }
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
     {
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
             : base(options)
         {
+        }
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+            builder.UseOpenIddict();
         }
     }
 }
