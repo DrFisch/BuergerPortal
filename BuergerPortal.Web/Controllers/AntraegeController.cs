7f80c5cbf831a5faa062b62814c78145c92b3058
Sponsel, Lukas (ZBFS) <lukas.Sponsel@zbfs.bayern.de>
2025-11-04 15:31:04 +0100

    ui neue views

---
+++ BuergerPortal.Web/Controllers/AntraegeController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BuergerPortal.Web.Controllers
+{
+    public class AntraegeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpGet]
+        public IActionResult Status() { return View(); }
+
+        [HttpGet]
+        public IActionResult NeuerReisepass() { return View(); }
+
+        [HttpGet]
+        public IActionResult NeuerSperrmuell() { return View(); }
+    }
+}
