e6368ad113f4f863f62d49fd6d339b16d736b0ac
Sponsel, Lukas (ZBFS) <lukas.Sponsel@zbfs.bayern.de>
2025-11-12 08:32:29 +0100

    settings frontend

---
+++ BuergerPortal.Web/Features/Settings/ViewModels/SettingsVm.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace BuergerPortal.Web.Features.Settings.ViewModels
+{
+    public sealed class SettingsVm
+    {
+        // Nur "Light" oder "Dark"
+        public string Theme { get; set; } = "Light";
+
+        // "de" | "en" (oder weitere)
+        public string Language { get; set; } = "de";
+        public IEnumerable<SelectListItem> Languages { get; set; } = Enumerable.Empty<SelectListItem>();
+
+        public bool PushEnabled { get; set; }
+        public bool ReduceDataUsage { get; set; }
+        public bool AnalyticsOptIn { get; set; }
+        public bool AllowGeolocation { get; set; }
+    }
+}
